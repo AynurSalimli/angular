2fd09e4ccd1334229dc8e03451810ce105dea438
AynurSalimli <aynur.selimli@ascca.edu.az>
2023-09-11 23:20:00 +0400

    two components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-styles:[`
-h3{
-  color: blue
-}`]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   name = 'my app';
