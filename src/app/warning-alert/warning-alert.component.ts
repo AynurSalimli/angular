b6e35d2a8aaf65148e4fe2703a48577218ef5078
AynurSalimli <aynur.selimli@ascca.edu.az>
2023-09-11 23:24:22 +0400

    two components

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-warning-alert',
   templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  styleUrls: ['./warning-alert.component.css'],
 })
 export class WarningAlertComponent {
 
