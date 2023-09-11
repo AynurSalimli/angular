82332c8756b42286542a7de75dd42e0a8dacce21
AynurSalimli <aynur.selimli@ascca.edu.az>
2023-09-12 00:00:35 +0400

    two components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+      templateUrl: "./servers.component.html",
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+allowNewServer = false;
 
+constructor(){
+  setTimeout(()=>{
+this.allowNewServer = true
+  },2000)
+}
 }
